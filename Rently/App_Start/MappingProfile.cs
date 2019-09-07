22666f471b8c0b6ebae5319451aa06765172ce7c
Balazs Martikán <martikanbalazs@outlook.com>
2019-09-07 11:19:16 +0200

    Building API for customers and Movies

---
+++ Rently/App_Start/MappingProfile.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using AutoMapper;
+using Rently.Dtos;
+using Rently.Models;
+
+namespace Rently.App_Start
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            
+            // Domain to Dto
+            //API -> Outbound
+            Mapper.CreateMap<Customer, CustomerDto>();
+            Mapper.CreateMap<Movie, MovieDto>();
+
+
+            // Dto to Domain
+            // API <- Inbound
+            Mapper.CreateMap<CustomerDto, Customer>()
+                .ForMember(c => c.Id, opt => opt.Ignore());
+
+            Mapper.CreateMap<MovieDto, Movie>()
+                .ForMember(c => c.Id, opt => opt.Ignore());
+
+
+        }
+    }
+}
---
+++ Rently/Dtos/MovieDto.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using Rently.Models;
+
+namespace Rently.Dtos
+{
+    public class MovieDto
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        public string Name { get; set; }
+
+        [Required]
+        public byte GenreTypeId { get; set; }
+
+        public DateTime ReleaseDate { get; set; }
+
+        public DateTime DateAdded { get; set; }
+
+        [Required]
+        public byte NumberInStock { get; set; }
+    }
+}
